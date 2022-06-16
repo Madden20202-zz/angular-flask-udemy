0d015379ad13809e6f4ff61ebb563fbf7f677ffb
austin <business.mocksock@gmail.com>
2022-06-16 17:31:22 -0500

    add in construtor and getPizzaInformation

--- src/app/pizza-orders/pizza-orders.model.ts
+++ src/app/pizza-orders/pizza-orders.model.ts
@@ -1,7 +1,16 @@
 export class PizzaOrder {
 
-    public crust: String
-    public size: String
-    public toppings: String
+    private crust: String
+    private size: String
+    private toppings: String
 
+    constructor(Crust: String, Size: String, Toppings:String) {
+        this.crust = Crust,
+        this.size = Size,
+        this.toppings = Toppings
+    }
+
+    getPizzaInformation(): String {
+        return this.crust, this.size, this.toppings
+    }
 }
