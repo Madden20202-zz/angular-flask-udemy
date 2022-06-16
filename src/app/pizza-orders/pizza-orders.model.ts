e4127f41ed696d61cf82ee632d966d8b9556d5e6
austin <business.mocksock@gmail.com>
2022-06-16 17:25:15 -0500

    last tutorial was old

--- src/app/pizza-orders/pizza-orders.model.ts
+++ src/app/pizza-orders/pizza-orders.model.ts
@@ -1,19 +1,7 @@
-export interface Deserializable {
-    deserialize(input: any): this
-}
-
 export class PizzaOrder implements Deserializable {
 
     public crust: String
     public size: String
     public toppings: String
 
-    deserialize(input: any): this {
-        return Object.assign(this, input)
-    }
-
-    showPizzaOrder() {
-        return this.crust, this.size, this.toppings
-    }
-    
 }
