53d81035516c36a1eab94b6089b6642a12d07cd2
austin <business.mocksock@gmail.com>
2022-06-16 10:51:03 -0500

    fill out use of api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-flask-udemy';
+  title = 'Pizza Orders';
+
+  pizzaOrders: any
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.getPizzaOrder()
+  }
+  
+  getPizzaOrder(): any {
+    this.api.getPizzaOrder().subscribe((data)=> {
+      this.pizzaOrders = data
+    })
+  }
 }
